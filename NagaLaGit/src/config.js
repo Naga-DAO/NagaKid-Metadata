6d26db867500707a83973ab1b4beeba3ff35592f
Nawapat Buakoet <nawapatb.2b@hotmail.com>
2022-06-21 21:54:30 +0700

    updated

--- NagaLaGit/src/config.js
+++ NagaLaGit/src/config.js
@@ -7,33 +7,26 @@ const network = NETWORK.eth;
 // General metadata for Ethereum
 const namePrefix = "Naga Kids";
 const description = "Test1";
-const baseUri = "ipfs://QmQbfz3rrELpKPdczAujrTqjx8Vt5HPWPGMhWXNXXy7TA5"; //CID ipfs://uri
+const baseUri = "ipfs://QmUoyF94YJKTKgNFwJgVRkZdQiHzPgPnpdmQ1PiXyMVP33"; //CID ipfs://uri
 
 const solanaMetadata = {
-  symbol: "Naga Kids",
-  seller_fee_basis_points: 1000, // Define how much % you want from secondary market sales 1000 = 10%
-  external_url: "https://www.youtube.com/watch?v=vFgmPOS5gTk&ab_channel=SomethingIhadneverdonebefore",
-  creators: [
-    {
-      address: "70x90d0949af536E45c6ccdbf64C6C6dF31c5B519eB", //metamask wallet ID
-      share: 100,
-    },
-  ],
+    symbol: "Naga Kid",
+    seller_fee_basis_points: 1000, // Define how much % you want from secondary market sales 1000 = 10%
+    external_url: "https://www.nagadaonft.com/",
+    creators: [
+        {
+            address: "70x90d0949af536E45c6ccdbf64C6C6dF31c5B519eB", //metamask wallet ID
+            share: 100,
+        },
+    ],
 };
 
 // If you have selected Solana then the collection starts from 0 automatically
 const layerConfigurations = [
-  {
-    growEditionSizeTo: 10, // Taget 1111
-    layersOrder: [
-      { name: "5BG" },
-      { name: "4NAGA" },
-      { name: "3body" },
-      { name: "2head" },
-      { name: "1mouse" },
-
-    ],
-  },
+    {
+        growEditionSizeTo: 100, // Taget 1111
+        layersOrder: [{ name: "Background" }, { name: "NagaKid" }, { name: "Body" }, { name: "Head" }, { name: "Mouse" }],
+    },
 ];
 
 const shuffleLayerConfigurations = false;
@@ -41,81 +34,85 @@ const shuffleLayerConfigurations = false;
 const debugLogs = false;
 
 const format = {
-  width: 512,   // Picture Size
-  height: 512,  // Picture Size 
-  smoothing: false,
+    width: 2000, // Picture Size
+    height: 2000, // Picture Size
+    smoothing: false,
 };
 
 const gif = {
-  export: false,
-  repeat: 0,
-  quality: 100,
-  delay: 500,
+    export: false,
+    repeat: 0,
+    quality: 100,
+    delay: 500,
 };
 
 const text = {
-  only: false,
-  color: "#ffffff",
-  size: 20,
-  xGap: 40,
-  yGap: 40,
-  align: "left",
-  baseline: "top",
-  weight: "regular",
-  family: "Courier",
-  spacer: " => ",
+    only: false,
+    color: "#ffffff",
+    size: 20,
+    xGap: 40,
+    yGap: 40,
+    align: "left",
+    baseline: "top",
+    weight: "regular",
+    family: "Courier",
+    spacer: " => ",
 };
 
 const pixelFormat = {
-  ratio: 2 / 128,
+    ratio: 2 / 128,
 };
 
 const background = {
-  generate: true,
-  brightness: "80%",
-  static: false,
-  default: "#000000",
+    generate: true,
+    brightness: "80%",
+    static: false,
+    default: "#000000",
 };
 
-const extraMetadata = {};
+const extraMetadata = {
+    external_url: "https://www.nagadaonft.com/",
+    // seller_fee_basis_points: 1000, // 100 = 1%
+    // fee_recipient: "0x6d644A42aCc9156437f20Ac002F1a73A74863Efa",
+};
 
 const rarityDelimiter = "#";
 
 const uniqueDnaTorrance = 10000;
 
 const preview = {
-  thumbPerRow: 5,
-  thumbWidth: 50,
-  imageRatio: format.height / format.width,
-  imageName: "preview.png",
+    thumbPerRow: 5,
+    thumbWidth: 50,
+    imageRatio: format.height / format.width,
+    imageName: "preview.png",
 };
 
 const preview_gif = {
-  numberOfImages: 5,
-  order: "ASC", // ASC, DESC, MIXED
-  repeat: 0,
-  quality: 100,
-  delay: 500,
-  imageName: "preview.gif",
+    numberOfImages: 5,
+    order: "ASC", // ASC, DESC, MIXED
+    repeat: 0,
+    quality: 100,
+    delay: 500,
+    imageName: "preview.gif",
 };
 
 module.exports = {
-  format,
-  baseUri,
-  description,
-  background,
-  uniqueDnaTorrance,
-  layerConfigurations,
-  rarityDelimiter,
-  preview,
-  shuffleLayerConfigurations,
-  debugLogs,
-  extraMetadata,
-  pixelFormat,
-  text,
-  namePrefix,
-  network,
-  solanaMetadata,
-  gif,
-  preview_gif,
+    format,
+    baseUri,
+    description,
+    background,
+    uniqueDnaTorrance,
+    layerConfigurations,
+    rarityDelimiter,
+    preview,
+    shuffleLayerConfigurations,
+    debugLogs,
+    extraMetadata,
+    pixelFormat,
+    text,
+    namePrefix,
+    network,
+    solanaMetadata,
+    gif,
+    preview_gif,
 };
